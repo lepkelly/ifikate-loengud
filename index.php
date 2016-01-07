8871a48fad462d1df27ed16a0dcc057a8c41d23c
Your Name <you@example.com>
2016-01-07 10:38:57 +0200

    index, header, footer lisamine

---
+++ index.php
@@ -0,0 +1,72 @@
+<?php require_once("header.php"); ?>
+
+
+	<nav class="navbar navbar-default navbar-fixed-top">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="#">
+						<img alt="Brand" src="./pics/logo_1899910_web.jpg">
+					</a>
+				</div>
+			</div>
+		</nav>
+	</nav>
+
+	<br><br><br><br><br><br><br><br><br>
+	
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <div class="col-sm-offset-1 col-sm-6">
+                <h1>Welcome to my site!</h1>
+            </div>
+            <div class="col-sm-offset-1 col-sm-3">
+            
+			
+			<form>
+              <div class="form-group">
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+              </div>
+              
+
+                <div class="row">
+                
+                    <div class="col-lg-8">
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                        </div>
+                    </div>
+					
+					                    
+                    <div class="col-lg-4 hidden-sm hidden-md">
+                        <button type="submit" class="btn btn-info btn-block">Login 1</button>
+                    </div>
+                    
+                    <div class="col-lg-4 hidden-lg hidden-xs pull-right">
+                        <button type="submit" class="btn btn-info">Login 2</button>
+                    </div>
+					                  
+                </div>
+               
+            </form>
+            </div>
+                    
+    
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php require_once("footer.php"); ?>
