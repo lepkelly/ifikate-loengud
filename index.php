552c0c90ddd0c64e97fb9727dcf607fe3005f5ac
Your Name <you@example.com>
2016-01-07 12:34:13 +0200

    index muudetud, lisatud login

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
-<?php require_once("header.php"); ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
 
 	<nav class="navbar navbar-default navbar-fixed-top">
 		<nav class="navbar navbar-default">
-			<div class="container-fluid">
+			<div class="container">
 				<div class="navbar-header">
 					<a class="navbar-brand" href="#">
 						<img alt="Brand" src="./pics/logo_1899910_web.jpg">
@@ -12,61 +21,117 @@
 			</div>
 		</nav>
 	</nav>
+<br><br><br><br>
 
-	<br><br><br><br><br><br><br><br><br>
-	
-    <div class="container-fluid">
-        <div class="jumbotron">
-            <div class="col-sm-offset-1 col-sm-6">
-                <h1>Welcome to my site!</h1>
-            </div>
-            <div class="col-sm-offset-1 col-sm-3">
-            
-			
-			<form>
-              <div class="form-group">
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-              </div>
-              
+<div class="container-fluid">
+<br>
 
-                <div class="row">
-                
-                    <div class="col-lg-8">
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                        </div>
-                    </div>
+	<div class="jumbotron">
+		<div class="row">
+			<div class="col-sm-8">
+				<h1>My First Bootstrap Page</h1>
+				<p>Resize this responsive page to see the effect!</p> 
+			</div>
+			<div class="col-sm-4"" style="background-color:white">
+				<br>
+				
+				
+				<div class="col-sm-12">
+				<br>
+		<form>
+			<div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			</div>
+				  
+
+			<div class="row">
 					
-					                    
-                    <div class="col-lg-4 hidden-sm hidden-md">
-                        <button type="submit" class="btn btn-info btn-block">Login 1</button>
-                    </div>
-                    
-                    <div class="col-lg-4 hidden-lg hidden-xs pull-right">
-                        <button type="submit" class="btn btn-info">Login 2</button>
-                    </div>
-					                  
-                </div>
-               
-            </form>
-            </div>
-                    
-    
-        </div>
+				<div class="col-lg-8">
+					<div class="form-group">
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					</div>
+				</div>
+						
+											
+				<div class="col-lg-4 hidden-sm hidden-md">
+					<button type="submit" class="btn btn-info btn-block">Login 1</button>
+					
+				</div>
+						
+				<div class="col-lg-4 hidden-lg hidden-xs pull-right">
+					<button type="submit" class="btn btn-info">Login 2</button>
+				</div>
+										  
+			</div>
+				   
+		</form>
+				</div>				  
+					
+				   
+				
+				
+			</div>
+			
+			
+			
+			
+				<div class="col-sm-offset-8 col-sm-4" style="background-color:white">
+				<h4>Ei ole kasutajat? Loo uus</h4>
+		<form>
+			<div class="form-group">
+				<input type="username" class="form-control" id="exampleInputUsername" placeholder="Username">
+			</div>
+		
+			<div class="form-group">
+				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			</div>
+				  
+
+			<div class="row">
+					
+				<div class="col-lg-8">
+					<div class="form-group">
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+					</div>
+				</div>
+						
+											
+				<div class="col-lg-4 hidden-sm hidden-md">
+					<button type="submit" class="btn btn-info btn-block">Create 1</button>
+				</div>
+						
+				<div class="col-lg-4 hidden-lg hidden-xs pull-right">
+					<button type="submit" class="btn btn-info">Create 2</button>
+				</div>
+										  
+			</div>
+				   
+		</form>
+	</div>
+			
+		</div>
+	</div>
+  
+
+
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Column 1</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
     </div>
-    
+    <div class="col-sm-4">
+      <h3>Column 2</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Column 3</h3>        
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+    </div>
+  </div>
+</div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php require_once("footer.php"); ?>
+</body>
+</html>
