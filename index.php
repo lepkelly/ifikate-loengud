78da5762a1c73e0704dfc52d853df2d14181f4cb
Your Name <you@example.com>
2016-01-08 11:21:34 +0200

    lisatud logimise ja kasutaja loomise php

--- index.php
+++ index.php
@@ -1,3 +1,87 @@
+<?php
+
+	//login error
+	$email_error = "";
+	$password_error = "";
+
+	
+	//create errorid
+	$new_username_error = "";
+	$new_email_error = "";
+	$new_password_error = "";
+	
+	//muutujad väärtustega
+	$new_username = "";
+	$new_email = "";
+	$new_password = "";
+
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+	
+		if(isset($_POST["create"])) {
+			//emaili kontroll
+			if(empty($_POST["new_email"])) {
+				$new_email_error = "ei saa olla tühi";
+			} else {
+				$new_email = test_input($_POST["new_email"]);
+			}
+						
+			//parooli kontroll
+			if(empty($_POST["new_password"])) {
+				$new_password_error = "sisesta parool";
+			} else {
+				$new_password = test_input($_POST["new_password"]);
+			
+				//parooli pikkuse kontroll
+				if(strlen($_POST["new_password"]) < 8 ){
+				
+					$new_password_error = "Peab olema vähemalt 8 sümbolit pikk";
+				}
+			}	
+		
+			if(	$new_email_error == "" && $new_password_error == ""){
+				echo hash("sha512", $new_password);
+				echo "Võib kasutajat luua! Kasutajanimi on ".$new_email." ja parool on ".$new_password;
+                
+		$hash = hash("sha512", $new_password);
+                
+				//nime kontroll
+				if (empty($_POST["new_username"])) {
+					$new_username_error = "Palun sisesta kasutajanimi";
+					} else {
+				$new_username = test_input($_POST["new_username"]);
+				}
+				
+		createUser($new_email, $hash, $new_username);
+				
+			
+			}
+		}
+	}
+		//üleliigse eemaldamine tekstist
+	function test_input($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,15 +116,21 @@
 				<h1>My First Bootstrap Page</h1>
 				<p>Resize this responsive page to see the effect!</p> 
 			</div>
+			
+			
+			<!--LOGIN-->
+			<!--LOGIN-->
+			<!--LOGIN-->
+			
 			<div class="col-sm-4"" style="background-color:white">
 				<br>
 				
 				
 				<div class="col-sm-12">
 				<br>
-		<form>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 			<div class="form-group">
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+				<input type="email" class="form-control" name="login_email" placeholder="Email"> <?php echo $email_error; ?>
 			</div>
 				  
 
@@ -48,18 +138,18 @@
 					
 				<div class="col-lg-8">
 					<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						<input type="password" class="form-control" name="login_password" placeholder="Password"> <?php echo $password_error; ?>
 					</div>
 				</div>
 						
 											
 				<div class="col-lg-4 hidden-sm hidden-md">
-					<button type="submit" class="btn btn-info btn-block">Login 1</button>
+					<button name="login" type="submit" class="btn btn-info btn-block">Login 1</button>
 					
 				</div>
 						
 				<div class="col-lg-4 hidden-lg hidden-xs pull-right">
-					<button type="submit" class="btn btn-info">Login 2</button>
+					<button name="login" type="submit" class="btn btn-info">Login 2</button>
 				</div>
 										  
 			</div>
@@ -74,16 +164,19 @@
 			
 			
 			
+			<!--CREATE-->
+			<!--CREATE-->
+			<!--CREATE-->
 			
 				<div class="col-sm-offset-8 col-sm-4" style="background-color:white">
 				<h4>Ei ole kasutajat? Loo uus</h4>
-		<form>
+		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
 			<div class="form-group">
-				<input type="username" class="form-control" id="exampleInputUsername" placeholder="Username">
+				<input type="username" class="form-control" name="new_username" placeholder="Username"> <?php echo $new_username_error; ?>
 			</div>
 		
 			<div class="form-group">
-				<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+				<input type="email" class="form-control" name="new_email" placeholder="Email"> <?php echo $new_email_error; ?>
 			</div>
 				  
 
@@ -91,17 +184,17 @@
 					
 				<div class="col-lg-8">
 					<div class="form-group">
-						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+						<input type="password" class="form-control" name="new_password" placeholder="Password"> <?php echo $new_password_error; ?>
 					</div>
 				</div>
 						
 											
 				<div class="col-lg-4 hidden-sm hidden-md">
-					<button type="submit" class="btn btn-info btn-block">Create 1</button>
+					<button name="create" type="submit" class="btn btn-info btn-block">Create 1</button>
 				</div>
 						
 				<div class="col-lg-4 hidden-lg hidden-xs pull-right">
-					<button type="submit" class="btn btn-info">Create 2</button>
+					<button name="create" type="submit" class="btn btn-info">Create 2</button>
 				</div>
 										  
 			</div>
